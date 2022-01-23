32c842ada1a055e753a268554afd0720ac9bcb13
vladuxa23 <vlad_1995.02@mail.ru>
2022-01-23 20:27:55 +0300

    refactor

--- car.py
+++ car.py
@@ -1,6 +1,11 @@
+import hashlib
+import random
 import time
 import uuid
 from driver import Driver
+import datetime
+
+from typing import Union
 
 
 class DriverTypeError(Exception):
@@ -15,13 +20,25 @@ class DriverNotFoundError(Exception):
     pass
 
 
+class DoTechnicalDiscussion(Exception):
+    pass
+
+
+class EngineIsRunning(Exception):
+    pass
+
+
+class DriverNoRules(Exception):
+    pass
+
+
 class Car:
     brand = None
     _max_speed = 180
     __created_car = 0
 
     def __init__(self, engine_type=None, body_type=None, gear_type=None,
-                 drive_type=None, configuration=None, color=None):
+                 drive_type=None, configuration=None, color=None, object_category=None):
 
         self.__body_type = body_type
         self._engine_type = engine_type
@@ -36,6 +53,12 @@ class Car:
 
         self.__status_engine = False
         self.__driver = None
+        self.__last_to = 0  # пробег на котором было сделано последнее ТО
+        self.__service_interval = 30  # необходимая частота ТО
+
+        self.car_key = None  # ключ который хранится в машине, и сверяется с ключом водителя
+        self.__keys_was_send = False  # переменная которая проверяет выдавались ли ключи
+        self.__object_category = object_category
 
     def __new__(cls, *args, **kwargs):
         cls.__append_new_car_counter()
@@ -57,8 +80,12 @@ class Car:
     def __append_new_car_counter(cls):
         cls.__created_car += 1
 
-    def start_engine(self):
-        self.__status_engine = True
+    def start_engine(self, key):
+        if self.__check_keys(key):
+            self.__status_engine = True
+            print('Машина завелась')
+        else:
+            print('Крутится стартер')
 
     def __is_ready_move(self):
         if not self.__status_engine:
@@ -68,16 +95,64 @@ class Car:
 
         return True
 
+    # def __is_ready_move(self):
+    #     if not self.__status_engine:
+    #         raise EngineIsNotRunning("двигатель не запущен")
+    #     if self.__driver is None:
+    #         raise DriverNotFoundError("водитель не найден")
+    #     if self.driver.rules is None:
+    #         raise DriverNoRules("без прав нельзя!")
+    #     if self.__object_category != self.driver.rules:
+    #         raise DriverNoRules("права водителя не подходят для этого ТС!")
+
+    def __create_keys(self):
+        h = hashlib.new('sha256')
+        vin = str(self.__vin_number).encode('utf-8')
+        h.update(vin)
+        self.car_key = h.hexdigest()
+        return self.car_key
+
+    def get_keys(self):
+        """Ключи выдаются только 1 раз"""
+        if self.__keys_was_send:
+            print('Ключи уже были выданы')
+        else:
+            self.__keys_was_send = True
+            return self.__create_keys()
+
+    def __check_keys(self, key):
+        if self.car_key == key:
+            return True
+        else:
+            print('Ключи не подходят')
+            return False
+
     def move(self, distance=10):
         try:
             if self.__is_ready_move():
                 for i in range(distance):
-                    print(f'Машина проехала {i + 1} км.')
-                    self.__mileage += 1
-                    time.sleep(0.3)
+                    if self.check_technical_discussion():
+                        self.move_direction(random.randint(0, 10))
+                        print(f'Машина проехала {i + 1} км.')
+                        self.__mileage += 1
+                        time.sleep(0.1)
+
+                        if self.__mileage % 20 == 0:
+                            a = 3
+                            print(f'Вам необходимо отдохнуть. Вы можете продолжить движение через {a} минут(ы)')
+                            time.sleep(a)
+
+                    else:
+                         raise DoTechnicalDiscussion("Срочно необходимо сделать ТО")
             print('Машина проехала указанный путь')
         except (EngineIsNotRunning, DriverNotFoundError) as e:
             print(f"Машина не может поехать, т.к. {e}")
+
+
+    @staticmethod
+    def move_direction(direction):
+        direction_dict = {0: "прямо", 1: "налево", 2: "направо", 3: "разворот"}
+        print(f"Автомобиль выполняет движение: {direction_dict.get(direction, 'прямо')}")
 
     @property
     def driver(self):
@@ -99,6 +174,27 @@ class Car:
                 f'Ожидается тип {int} или {float}, получен {type(mileage)}')
         self.__mileage = mileage
 
+    def check_technical_discussion(self) -> bool:
+        if self.__mileage - self.__last_to == 40:
+            return False
+        elif self.__mileage - self.__last_to == 20:
+            print(f"Необходимо сделать ТО через 10 км")
+        elif self.__mileage - self.__last_to == self.__service_interval:
+            print(f"Сейчас необходимо сделать ТО")
+        return True
+
+    def do_technical_discussion(self):
+        self.__last_to = self.__mileage
+        print("Очередное ТО пройден")
+
+    def autostart_engine(self):
+        if not self.__status_engine:
+            self.__status_engine = True
+            return True
+        else:
+            EngineIsRunning("двигатель уже запущен")
+            return False
+
 
 class Honda(Car):
     brand = "Honda"
@@ -110,51 +206,16 @@ class Honda(Car):
 
 
 if __name__ == '__main__':
-    pass
-    # car = Car('бензин', 'седан', 'автомат', 'полный', 'люкс', 'белый')
-    # car_2 = Car('бензин', 'седан', 'автомат', 'полный', 'люкс', 'черный')
-    #
-    # honda1 = Honda('бензин', 'седан', 'автомат', 'полный', 'люкс', 'черный')
-    # honda2 = Honda('бензин', 'седан', 'автомат', 'полный', 'люкс', 'черный')
-    #
-    # Car.change_brand("Kia")
-    #
-    # print(honda1.brand)
+    # pass
+    car = Car('бензин', 'седан', 'автомат', 'полный', 'люкс', 'белый')
+    # driver_key = car.get_keys()
+    # car.start_engine(driver_key)
 
-    # print(car.brand)
-    # print(car_2.brand)
-    # Car.change_brand("Nissan")
-    # print(car.brand)
-    # print(car_2.brand)
-
-    # print(car._max_speed)
-    # print(car_2._max_speed)
-    # Car._set_max_speed(190)
-    # print(car._max_speed)
-    # print(car_2._max_speed)
-
-
-
-
-    # Блок работы с защищёнными методами
-    # car.start_engine()
-    # car.driver = Driver('Иван')
-    # car.driver = Driver('Иван')
-    # # Блок работы с методами
-    # car.move()
-    # print(car.get_mileage())
-    # car.move()
-    # print(car.get_mileage())
-    #
-    # car._set_mileage(30)
-    # print(car.get_mileage())
-    # car.move()
-    # print(car.get_mileage())
-    # car._set_mileage(10)
-    # print(car.get_mileage())
-
-    # Блок сеттеров
-    # car.driver = Driver('Иван')
-    # print(car.driver)
-    # car.set_driver(Driver('Иван'))
-    # print(car.get_driver())
+    start_time = datetime.time(15, 47, 0, 0)
+    while True:
+        a = datetime.datetime.now().time()
+        print(a)
+        print(start_time)
+        if a > start_time:
+            print(car.autostart_engine())
+            break
--- driver.py
+++ driver.py
@@ -1,17 +1,48 @@
+from rules import Categories
+from typing import Optional
+
+
 class Driver:
-    def __init__(self, name):
+    def __init__(self, name, age: int, rules: Optional[Categories] = None):
         self.name = name
+        self.age = age
+        self.rules = rules
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age: int):
+        if not isinstance(age, int):
+            raise TypeError('Задайте возраст целочисленным значением')
+        if age < 0:
+            raise ValueError("Возраст должен иметь положительное значение!")
+        self._age = age
+
+    @property
+    def rules(self):
+        return self._rules
+
+    @rules.setter
+    def rules(self, cat: str):
+        if not (cat is None):
+            self._rules = Categories(self.age, cat)
+        else:
+            self._rules = None
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.name})"
+        return f"{self.__class__.__name__}({self.name},{self.age},{self.rules})"
 
     def __str__(self):
-        return f"Водитель {self.name}"
+        return f"Водитель {self.name}, возраст {self.age}, права {self.rules}"
 
 
 if __name__ == '__main__':
-    driver_vasya = Driver("Василий")
-    driver_ivan = Driver("Иван")
+    driver_vasya = Driver("Василий", 18)
+    driver_ivan = Driver("Иван", 14)
 
     print(driver_vasya)
     print(driver_ivan)
+
+
