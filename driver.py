1be843ccf1c32ee23db800e09a1fb8a0263a6245
Alex <alx@mail.ru>
2022-01-25 17:46:10 +0300

    driver

--- driver.py
+++ driver.py
@@ -7,11 +7,8 @@ class Driver:
     """
     Класс описывающий среднестатистического водителя
     """
-
-    TWO_YEAR = 730           # два года в днях
-    MAX_NOOB_SPEED = 70      # максимальная скорость для новичков
     MAX_MOVE_NOOB_TIME = 120 # максимальное время в пути для новичка 2 ч
-    MAX_SPEED = 130          # максимальная скорость на территории РФ
+
     MAX_MOVE_TIME = 360      # максимальная время в пути для любого водителя без перерыва 4 ч
 
     def __init__(self, name, age: int, rules: Optional[Categories] = None,
@@ -22,53 +19,152 @@ class Driver:
         self.rules = rules
         self.experience = experience
 
-        self.max_speed = self.get_max_speed()
-        self.max_move_time = self.get_max_time_in_move()
-        self.max_distance = self.max_speed * (self.max_move_time * 60)
+        #  установка "начальных значений
 
-        self._max_country_speed = None
+        self._set_max_allowed_speed_in_counry()  # установка в self._max_country_speed
+                                                 # максимально допустимой скорости в стране
+        self._set_max_noob_speed()               # установка в self._max_noob_country_speed
+                                                 # ограничения максимальной скорости для неопытных водителей в стране
+        self._set_test_roll_in_country()         # установка в self._test_roll
+                                                 # величины "испытательного" срока для молодых водтителей
+        self._set_max_time_behind_wheel()        # установка максимального времени за рулем для всех в
+                                                 # _max_time_behind_wheel
+        self._set_max_noob_time_behind_wheel()   # установка максимального времени за рулем для неопытных
+                                                 # в _max_noob_time_behind_wheel
 
-    @property
-    def max_allowed_speed_in_counry(self):
+    def _set_max_time_behind_wheel(self, new_value: int = 360) -> None:
+        """
+        Установка максимальнгого времени за рулем для всех (без отдыха)
+        :param new_value: новое значение в минутах (по умолчанию 360 мин)
+        """
+        self.__chek_time_of_wheel(new_value)
+        self._max_time_behind_wheel = new_value
+
+    def get_max_time_behind_wheel(self) -> int:
+        """
+        Получение максимальнгого времени за рулем для всех (без отдыха)
+        :return: значение в минутах
+        """
+        return self._max_time_behind_wheel
+
+    def _set_max_noob_time_behind_wheel(self, new_value = 120) -> None:
+        """
+        установка максимального времени без отдыха за рулем для новичков
+        :param new_value: новое значение в минутах (по умолчанию - 120)
+        """
+        self.__chek_time_of_wheel(new_value)
+        self._max_noob_time_behind_wheel = new_value
+
+    def get_max_noob_time_behind_wheel(self) -> int:
+        """
+        Возвращает максимально допустимое время за рулем для новичка
+        :return: максимальное время за рулем в минутах
+        """
+        return self._max_noob_time_behind_wheel
+
+    @staticmethod
+    def __chek_time_of_wheel(value) -> None:
+        """
+        проверка введенного времени на соответствие
+        """
+        if not isinstance(value, int):
+            raise TypeError('Задайте время целочисленным значением (в минутах)')
+        if (value <= 0) or (value >= 24 * 60):
+            raise ValueError('Больше суток без перерыва никому ехать нельзя!')
+
+    def _set_test_roll_in_country(self, new_value = 2) -> None:
+        """
+        установка нового значения "испытательного срока" для молодых водителей
+        (по умолчанию - 2 года)
+        :param new_value: новое значение в годах
+        """
+        if not isinstance(new_value, int):
+            raise TypeError('Задайте испытательный срок целочисленным значением')
+        if (new_value < 0) or (new_value >= 50):
+            raise ValueError('Проверьте вводимые для испытательного срока значения!')
+        self._test_roll = new_value
+
+    def get_test_roll_in_country(self) -> int:
+        """
+        возвращает значение испытательного срока в стране
+        """
+        return self._test_roll
+
+    def get_max_noob_speed(self) -> int:
+        """
+        возвращает максимально допустимую максимально допустимую скорость
+        для неопытных водителей в стране
+        """
+        return self._max_noob_country_speed
+
+    def _set_max_noob_speed(self, new_value: int = 70):
+        """
+        устанавливает значение максимальной скорости для неопытных водителей
+        (по умолчанию - 70 км/ч)
+        :param new_value: новое значение максимальной скорости (км/ч)
+        """
+        self.__chek_speed_value(new_value)
+        self._max_noob_country_speed = new_value
+
+    @staticmethod
+    def __chek_speed_value(value) -> None:
+        """проверка соответсвия значения скорости"""
+        if not isinstance(value, int):
+            raise TypeError('Задайте скорость целочисленным значением')
+        if value <= 0:
+            raise ValueError('Задайте максимальную скорость положительным значением!')
+
+    def get_max_allowed_speed_in_counry(self):
         """
         возвращает максимально допустимую скорость в стране
         """
         return self._max_country_speed
 
-    @max_allowed_speed_in_counry.setter
-    def _max_allowed_speed_in_counry(self, new_value: int = 130):
+    def _set_max_allowed_speed_in_counry(self, new_value: int = 130):
         """
         устанавливает максимально допустимую скорость в стране
+        (по умолчанию = 130 км/ч)
         :param new_value: новое значение максимальной скорости
         """
-        if not isinstance(new_value, int):
-            raise TypeError('Задайте скорость целочисленным значением')
-        if new_value <= 0:
-            raise ValueError('Задайте максимальную скорость положительным значением!')
+        self.__chek_speed_value(new_value)
         self._max_country_speed = new_value
 
-    def get_max_speed(self):
-        return self.MAX_NOOB_SPEED if self.experience.day < self.TWO_YEAR else self.MAX_SPEED
+    def get_max_allowed_speed(self) -> int:
+        """
+        Возвращает максимально разрешенную скорость для водителя в зависимости от стажа
+        и ограничений в стране
+        :return: максимально разрешенная скорость в км/ч
+        """
+        return self.get_max_noob_speed() if self.experience.days < (self.get_test_roll_in_country() / 365) \
+            else self.get_max_allowed_speed_in_counry()
 
-    def get_max_time_in_move(self):
-        return self.MAX_MOVE_NOOB_TIME if self.experience.day < self.TWO_YEAR else self.MAX_MOVE_TIME
+    def get_max_time_in_move(self) -> int:
+        """
+        Возвращает максимальное время за рулем для водителя в зависимости от стажа установленных ограничений
+        :return: время в минутах
+        """
+        return self.get_max_noob_time_behind_wheel() if self.experience.days < (self.get_test_roll_in_country() / 365) \
+            else self.get_max_time_behind_wheel()
 
     @property
     def experience(self) -> datetime.timedelta:
         """
         возвращает стаж вождения как timedelta между текущей датой и датой выдачи ВУ
         """
-        return (datetime.datetime.now() - self._experience)
+        return datetime.datetime.now() - self._experience
 
     @experience.setter
     def experience(self, start_date: datetime):
         """
         установка даты начала отсчета стажа вождения
-        :param start_date: дата выдачи ВУ
+        :param start_date: дата выдачи ВУ ( в формате %d.%m.%Y)
         """
-        if not isinstance(start_date, datetime):
+        if isinstance(start_date, str):
+            start_date += ' 00:00'
+            start_date = datetime.datetime.strptime(start_date, '%d.%m.%Y %H:%S')
+        if not isinstance(start_date, datetime.date):
             TypeError('введите корректную дату')
-        if abs(start_date - datetime.datetime.now()) > self.age:
+        if abs((start_date - datetime.datetime.now()).days) > self.age * 365:
             raise ValueError('Стаж больше возраста?')
         self._experience = start_date
 
@@ -105,15 +201,25 @@ class Driver:
 
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.name},{self.age},{self.rules})"
+        return f"{self.__class__.__name__}({self.name},{self.age},{self.rules}, {self.experience})"
 
     def __str__(self):
-        return f"Водитель {self.name}, возраст {self.age}, права {self.rules}"
+        if self.experience.days // 365 > 0:
+            return f"Водитель {self.name}, возраст {self.age}, права {self.rules}, " \
+                   f"стаж {self.experience.days // 365} лет"
+        elif self.experience.days // 30 > 0:
+            return f"Водитель {self.name}, возраст {self.age}, права {self.rules}, " \
+                   f"стаж {self.experience.days // 30} месяц(ев)"
+        else:
+            return f"Водитель {self.name}, возраст {self.age}, права {self.rules}, " \
+                   f"стаж {self.experience.days} дней"
 
 
 if __name__ == '__main__':
-    driver_vasya = Driver("Василий", 18, 'М')
+    driver_vasya = Driver("Василий", 56, 'М', "01.10.2000")
     driver_ivan = Driver("Иван", 14)
 
     print(driver_vasya)
+    print(driver_vasya.get_max_time_in_move())
+    print(driver_vasya.experience)
     print(driver_ivan)
